c410ab0b8ca2258e16603a8d259f8f44e511a973
Azadx69x <nezukojerry803@gmail.com>
2025-12-03 12:34:35 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,24 +1,15 @@
-/**
-
-@author Azad
-
-Command: out
-
-Description: Makes the bot leave the group if the owner requests it (with a farewell message).
-*/
-
 module.exports = {
 config: {
 name: "out",
 version: "1.1",
-author: "Azad",
+author: "Azadx69x",
 countDown: 5,
 role: 2,
 shortDescription: {
-en: "Kick 🦶 bot from group (owner only)"
+en: "bot out from group owner only"
 },
 longDescription: {
-en: "Remove the bot from the group (only owner can use). Sends a message before leaving."
+en: "out the bot from the group"
 },
 category: "owner",
 guide: {
@@ -27,19 +18,17 @@ en: "just type: out"
 },
 
 onStart: async function ({ api, message, event }) {
-const ownerIDs = ["61578365162382"]; // put your Facebook UID(s) here
+const ownerIDs = ["61578365162382"];
 
 if (!ownerIDs.includes(event.senderID)) {
 return api.sendMessage(
-"Khanakiar chele, amake bahir korar tui ke ? 🤬😤",
+"Khanakiar pola,tui ke re ? 🤬",
 event.threadID,
 event.messageID
 );
 }
 
-// Send a farewell message first
 api.sendMessage("azad vai bollo left nite tai ami ar thakte parbo na bye Allah Hafez,👋🙂", event.threadID, () => {
-// Leave after 2 seconds
 setTimeout(() => {
 api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
 }, 2000);
